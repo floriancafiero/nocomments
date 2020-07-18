853e5401de0af6eb26f3699d16612d0167374c65
Florian Cafiero <florian.cafiero@polytechnique.edu>
2020-07-18 21:48:04 +0200

    Create internal_links_identification.R

---
+++ internal_links_identification.R
@@ -0,0 +1,28 @@
+#Fonctions d’identification des liens “internes”
+#Remarque : l’utilisation des préfixes = liées à hyphe mais adaptable à d’autres corpus.
+#Requires library : stringr
+#Requires files : data.frame htmls_edges (source, target, id) - data.frame prefixes (id_site, multi_prefixes, n_prefixes, name, prefixe 1… prefixe Nmax)
+#Products : data.frame htmls_edges_IN (source, target, weight)
+
+sort_edges <- function(htmls_edges, prefixes)
+{
+htmls_edges_IN <- htmls_edges
+sites = prefixes$id_site
+for (x in 1:length(sites))
+{
+prefixe <- prefixes[which(prefixes$id_site == sites[x]),]
+INx <- c()
+for (y in 5:(4+prefixe$n_prefixes)) # passage par les indices de colonnes en raison de la structure du fichier
+{
+a <- grep(tolower(prefixe[y]),tolower(htmls_edges$target))
+INx <- c(INx,a)
+htmls_edges_IN[a,2] <- sites[x]
+}
+IN=c(IN,unique(INx))
+}
+htmls_edges_IN <- htmls_edges_IN[IN,]
+htmls_edges_IN <- htmls_edges_IN[which(htmls_edges_IN$source != htmls_edges_IN$target),]
+htmls_edges_IN <- as.data.frame(table(htmls_edges_IN[,1],htmls_edges_IN[,2]))
+names(htmls_edges_IN) <- c(“source”, “target”, “weight”)
+return(htmls_edges_IN)
+}
